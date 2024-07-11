fe63bba8e120da4dcb565bef9115e18e67651d24
lindsayshuo <932695342@qq.com>
2024-07-11 17:00:10 +0800

    Update image_utils.h

--- utils/image_utils.h
+++ utils/image_utils.h
@@ -17,23 +17,12 @@ typedef struct {
     float scale;
 } letterbox_t;
 
-/**
- * @brief Read image file (support png/jpeg/bmp)
- * 
- * @param path [in] Image path
- * @param image [out] Read image
- * @return int 0: success; -1: error
- */
-int read_image(const char* path, image_buffer_t* image);
 
-/**
- * @brief Write image file (support jpg/png)
- * 
- * @param path [in] Image path
- * @param image [in] Image for write (only support IMAGE_FORMAT_RGB888)
- * @return int 0: success; -1: error
- */
-int write_image(const char* path, const image_buffer_t* image);
+int read_image(const std::string& image_path, image_buffer_t* image_buffer);
+
+
+int write_image(const std::string& image_path, const image_buffer_t* image_buffer);
+
 
 /**
  * @brief Convert image for resize and pixel format change
